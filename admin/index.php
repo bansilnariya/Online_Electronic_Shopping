47c03de16be96f11fa5a1eb7a0448fd74589bd78
BANSIL NARIYA <145281335+bansilnariya@users.noreply.github.com>
2024-02-29 17:43:34 +0530

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -27,7 +27,7 @@
             <div id="layoutSidenav_content">
                 <main>
                     <div class="container-fluid px-4">
-                        <h1 class="mt-4">Croma Shope Dashboard</h1>
+                        <h1 class="mt-4">Electronic Shope Dashboard</h1>
                         <ol class="breadcrumb mb-4">
                             <li class="breadcrumb-item active">Dashboard</li>
                         </ol>
